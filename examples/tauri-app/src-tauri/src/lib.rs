78fa498a67b82143b56b78ac8e98027a324aa88c
lloydzhou <lloydzhou@qq.com>
2025-02-14 13:51:26 +0800

    增加rust代码和js代码

--- examples/tauri-app/src-tauri/src/lib.rs
+++ examples/tauri-app/src-tauri/src/lib.rs
@@ -1,13 +1,10 @@
 // Learn more about Tauri commands at https://v2.tauri.app/develop/calling-rust/#commands
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+mod mcp;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![mcp::mcpstart, mcp::mcpmessage, mcp::mcpstop])
         .plugin(tauri_plugin_mcp_transport::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
